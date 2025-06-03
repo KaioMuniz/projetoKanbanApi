aa86b344a2431d0d281be91e5556393e141fccd8
KaioMuniz <kkaioribeiro@gmail.com>
2025-06-03 14:37:55 -0300

    mapeamento orm 2

--- src/main/java/br/com/cotiinformatica/entities/TarefaKanban.java
+++ src/main/java/br/com/cotiinformatica/entities/TarefaKanban.java
@@ -4,10 +4,14 @@ import java.util.Date;
 import java.util.UUID;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table (name = "tarefas_kanban")
 public class TarefaKanban {
 
 	@Id
