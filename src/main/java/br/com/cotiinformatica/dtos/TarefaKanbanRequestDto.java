357c38e6b21549ad802073d5efe8b75f128c109c
KaioMuniz <kkaioribeiro@gmail.com>
2025-06-03 15:24:49 -0300

    TarefaKanbanResponseDto

--- src/main/java/br/com/cotiinformatica/dtos/TarefaKanbanRequestDto.java
+++ src/main/java/br/com/cotiinformatica/dtos/TarefaKanbanRequestDto.java
@@ -4,6 +4,7 @@ import java.util.Date;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public class TarefaKanbanRequestDto {
@@ -20,6 +21,7 @@ public class TarefaKanbanRequestDto {
 	private String responsavel;
 	
 	@NotNull(message = "A data prevista para entrega da tarefa é obrigatória.")
+	@Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "A data prevista para entrega deve estar no formato YYYY-MM-DD.")
 	private Date dataPrevistaEntrega;
 	
 	@NotNull(message = "O status de iniciado da tarefa é obrigatório.")
--- src/main/java/br/com/cotiinformatica/dtos/TarefaKanbanResponseDto.java
+++ src/main/java/br/com/cotiinformatica/dtos/TarefaKanbanResponseDto.java
@@ -1,6 +1,85 @@
 package br.com.cotiinformatica.dtos;
 
+import java.util.Date;
+import java.util.UUID;
+
 public class TarefaKanbanResponseDto {
+	
+	private UUID id;
+	private String titulo;
+	private String descricao;
+	private String responsavel;
+	private Date dataCriacao;
+	private Date dataPrevistaEntrega;
+	private Boolean iniciado;
+	private Boolean finalizado;
+	
+	
+	public TarefaKanbanResponseDto() {
+		super();
+	}
+	
+	public TarefaKanbanResponseDto(UUID id, String titulo, String descricao, String responsavel, Date dataCriacao,
+			Date dataPrevistaEntrega, Boolean iniciado, Boolean finalizado) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.descricao = descricao;
+		this.responsavel = responsavel;
+		this.dataCriacao = dataCriacao;
+		this.dataPrevistaEntrega = dataPrevistaEntrega;
+		this.iniciado = iniciado;
+		this.finalizado = finalizado;
+	}
+	
+	public UUID getId() {
+		return id;
+	}
+	public void setId(UUID id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getResponsavel() {
+		return responsavel;
+	}
+	public void setResponsavel(String responsavel) {
+		this.responsavel = responsavel;
+	}
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+	public Date getDataPrevistaEntrega() {
+		return dataPrevistaEntrega;
+	}
+	public void setDataPrevistaEntrega(Date dataPrevistaEntrega) {
+		this.dataPrevistaEntrega = dataPrevistaEntrega;
+	}
+	public Boolean getIniciado() {
+		return iniciado;
+	}
+	public void setIniciado(Boolean iniciado) {
+		this.iniciado = iniciado;
+	}
+	public Boolean getFinalizado() {
+		return finalizado;
+	}
+	public void setFinalizado(Boolean finalizado) {
+		this.finalizado = finalizado;
+	}
 	
 	
 
